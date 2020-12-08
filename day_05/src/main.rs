28f9093ef3c4478d7052a296d3bc1c65fd1434b4
Sander Vervaeke <sander_vervaeke@outlook.be>
2020-12-08 23:30:42 +0100

    day 05 part 02

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -10,25 +10,41 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Answer 1: {:?}", part_01(&boarding_passes));
     println!("Completed in {:?}", start.elapsed());
 
+    println!("Answer 2: {:?}", part_02(&boarding_passes));
+    println!("Completed in {:?}", start.elapsed());
+
     Ok(())
 }
 
-fn part_01(seats: &Vec<String>) -> u16 {
-    seats
+fn part_01(passes: &Vec<String>) -> u16 {
+    passes
             .iter()
             .map(|x| calculate_seat(x))
             .max()
             .unwrap()
 }
 
-fn part_02(seats: &Vec<String>) {
+fn part_02(passes: &Vec<String>) -> u16 {
+    let mut seats:Vec<u16> = passes
+            .iter()
+            .map(|x| calculate_seat(x))
+            .collect();
+            
+    seats.sort_unstable();
 
+    let result = (seats[0]..=seats[seats.len() - 1])
+        .zip(seats
+        .iter())
+        .find(|(expected, seat)| expected != *seat)
+        .unwrap();
+    
+    result.0
 }
 
 fn calculate_seat (boarding_pass: &String) -> u16
 {
     boarding_pass.chars().fold(0,  |acc, character| {
-        (acc << 1) | if character == 'B' || character == 'R' { 1 } else { 0 }
+        (acc << 1) | matches!(character, 'B' | 'R') as u16
     })
 }
 
