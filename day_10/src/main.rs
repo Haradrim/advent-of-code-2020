cf377ed997b6139040af08eb54eb9f372f47a1b7
Sander Vervaeke <sander_vervaeke@outlook.be>
2021-01-03 22:06:33 +0100

    day_10 part_02

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -8,6 +8,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Answer 1: {:?}", part_01(&voltage_ratings));
     println!("Completed in {:?}", start.elapsed());
 
+    let start = Instant::now();
+
+    println!("Answer 2: {:?}", part_02(&voltage_ratings));
+    println!("Completed in {:?}", start.elapsed());
+
     Ok(())
 }
 
@@ -17,9 +22,7 @@ fn part_01(voltage_ratings: &Vec<u64>) -> u64 {
     let mut count_3: u64 = 0;
 
     ratings.push(0);
-
     ratings.sort();
-
     ratings.push(ratings[ratings.len() - 1] + 3);
 
     let mut index = 0;
@@ -34,6 +37,30 @@ fn part_01(voltage_ratings: &Vec<u64>) -> u64 {
     }
 
     count_1 * count_3
+}
+
+fn part_02(voltage_ratings: &Vec<u64>) -> Option<u64> {
+    let mut ratings: Vec<u64> = voltage_ratings.iter().copied().collect();
+
+    ratings.push(0);
+    ratings.sort();
+
+    let mut methods: Vec<u64> = vec![0u64; ratings.len()];
+    methods[0] = 1;
+
+    for (start_index, start_rating) in ratings.iter().enumerate() {
+        let value = methods[start_index];
+
+        for (index, rating) in ratings[start_index..].iter().enumerate().skip(1) {
+            if rating - start_rating <= 3 {
+                methods[start_index + index] += value;
+            } else {
+                break;
+            }
+        }
+    }
+
+    methods.last().copied()
 }
 
 fn read_file(filename: &str) -> std::io::Result<Vec<u64>> {
@@ -51,5 +78,12 @@ mod tests {
         let ratings = read_file("example.txt").unwrap();
 
         assert_eq!(part_01(&ratings), 220);
+    }
+
+    #[test]
+    fn example_02() {
+        let ratings = read_file("example.txt").unwrap();
+
+        assert_eq!(part_02(&ratings), Some(19208));
     }
 }
